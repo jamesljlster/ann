9e1c9bd9fa7b82ae6a7c5a74d060665d32dcf8e9
Zheng-Ling Lai <jamesljlster@gmail.com>
2016-12-16 05:02:50 +0800

    Add find sub id function

--- src/ann_file_header.h
+++ src/ann_file_header.h
@@ -8,7 +8,9 @@ enum ANN_FILE_HEADER_LIST
 	ANN_HEADER_TRAINING_INFO,
 	ANN_HEADER_TOTAL_NODE,
 	ANN_HEADER_THRESHOLD_VALUE,
-	ANN_HEADER_WEIGHT_FACTOR
+	ANN_HEADER_WEIGHT_FACTOR,
+
+	ANN_HEADER_HEADER_LIST_COUNT
 };
 
 enum ANN_HEADER_APPLICATION_LIST
@@ -21,13 +23,17 @@ enum ANN_HEADER_TOPOLOGY_LIST
 	ANN_HEADER_TOPOLOGY_INPUTS,
 	ANN_HEADER_TOPOLOGY_OUTPUTS,
 	ANN_HEADER_TOPOLOGY_LAYERS,
-	ANN_HEADER_TOPOLOGY_TRANSFER_FUNC
+	ANN_HEADER_TOPOLOGY_TRANSFER_FUNC,
+
+	ANN_HEADER_TOPOLOGY_LIST_COUNT
 };
 
 enum ANN_HEADER_TRAINING_INFO_LIST
 {
 	ANN_HEADER_TRAINING_INFO_LEARNING_RATE,
 	ANN_HEADER_TRAINING_INFO_MOMENTUM_COEF,
+
+	ANN_HEADER_TRAINING_INFO_LIST_COUNT
 };
 
 enum ANN_HEADER_TOTAL_NODE_LIST
@@ -46,5 +52,22 @@ enum ANN_HEADER_WEIGHT_LIST
 };
 
 extern char* ann_file_header[];
+extern char* ann_header_application[];
+extern char* ann_header_topology[];
+extern char* ann_header_training_info[];
+extern char* ann_header_total_node[];
+extern char* ann_header_threshold_value[];
+extern char* ann_header_weight_factor[];
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int ann_get_topology_content_id(char* src);
+int ann_get_training_content_id(char* src);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
