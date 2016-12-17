e73ec2653b82de76b645d89643a1ce9e6cece298
Zheng-Ling Lai <jamesljlster@gmail.com>
2016-12-17 19:06:42 +0800

    Modify function prototype

--- src/ann.h
+++ src/ann.h
@@ -26,16 +26,17 @@ typedef void* ann_config_t;
 extern "C" {
 #endif
 
-int ann_create(ann_t* annPtr, int inputs, int outputs, int hiddenLayers, ...);
-int ann_create_by_config(ann_t* annPtr, ann_config_t config);
+int ann_create_args(ann_t* annPtr, int inputs, int outputs, int hiddenLayers, ...);
+int ann_create(ann_t* annPtr, ann_config_t config);
 int ann_import(ann_t* annPtr, const char* filePath);
 int ann_export(ann_t ann, const char* filePath);
 void ann_delete(ann_t ann);
 
-int ann_config_create(ann_config_t* configPtr, int inputs, int outputs);
+int ann_config_create(ann_config_t* configPtr, int inputs, int outputs, int tFuncIndex);
 int ann_config_import(ann_config_t* configPtr, const char* filePath);
 int ann_config_export(ann_config_t config, const char* filePath);
-int ann_config_add_hidden_layer(ann_config_t config, int nodes);
+int ann_config_set_layers(ann_config_t config, int hiddenLayers);
+int ann_config_set_nodes(ann_config_t config, int hiddenLayerIndex, int nodes);
 
 void ann_config_delete(ann_config_t config);
 void ann_config_print(ann_config_t config);
