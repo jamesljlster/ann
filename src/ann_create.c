dbc15568e6d03295d609b120fcf527e1af167080
Zheng-Ling Lai <jamesljlster@gmail.com>
2016-12-19 08:41:27 +0800

    Implement ann_config_create()

--- src/ann_create.c
+++ src/ann_create.c
@@ -3,4 +3,19 @@
 #include "ann.h"
 #include "ann_private.h"
 
+int ann_config_create(ann_config_t* configPtr)
+{
+	struct ANN_CONFIG_STRUCT* cfgRef;
+	
+	cfgRef = malloc(sizeof(struct ANN_CONFIG_STRUCT));
+	if(cfgRef == NULL)
+	{
+		return ANN_MEM_FAILED;
+	}
+	
+	ann_config_zeromem(cfgRef);
 
+	configPtr = cfgRef;
+	
+	return ANN_NO_ERROR;
+}
