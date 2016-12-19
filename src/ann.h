c4024b62d0ed0249c8c53f9aa2b60d756e665207
Zheng-Ling Lai <jamesljlster@gmail.com>
2016-12-19 08:34:14 +0800

    Finish config setting function

--- src/ann.h
+++ src/ann.h
@@ -33,11 +33,15 @@ int ann_import(ann_t* annPtr, const char* filePath);
 int ann_export(ann_t ann, const char* filePath);
 void ann_delete(ann_t ann);
 
-int ann_config_create(ann_config_t* configPtr, int inputs, int outputs, int tFuncIndex);
+//int ann_config_create(ann_config_t* configPtr, int inputs, int outputs, int tFuncIndex);
+int ann_config_create(ann_config_t* configPtr);
 int ann_config_import(ann_config_t* configPtr, const char* filePath);
 int ann_config_export(ann_config_t config, const char* filePath);
+int ann_config_set_inputs(ann_config_t config, int inputs);
+int ann_config_set_outputs(ann_config_t config, int outputs);
 int ann_config_set_layers(ann_config_t config, int hiddenLayers);
 int ann_config_set_nodes(ann_config_t config, int hiddenLayerIndex, int nodes);
+int ann_config_set_transfer_func(ann_config_t config, int tFuncIndex);
 
 void ann_config_delete(ann_config_t config);
 void ann_config_print(ann_config_t config);
--- src/ann_set.c
+++ src/ann_set.c
@@ -1,7 +1,101 @@
+#include <stdlib.h>
 #include <assert.h>
 
 #include "ann.h"
 #include "ann_private.h"
+
+#include "debug.h"
+
+#define INIT_NODES	10
+
+int ann_config_set_inputs(ann_config_t config, int inputs)
+{
+	struct ANN_CONFIG_STRUCT* cfgRef = config;
+	
+	cfgRef->inputs = inputs;
+
+	return ANN_NO_ERROR;
+}
+
+int ann_config_set_outputs(ann_config_t config, int outputs)
+{
+	struct ANN_CONFIG_STRUCT* cfgRef = config;
+	
+	cfgRef->outputs = outputs;
+
+	return ANN_NO_ERROR;
+}
+
+int ann_config_set_layers(ann_config_t config, int hiddenLayers)
+{
+	int i;
+	int retValue = ANN_NO_ERROR;
+	int tmpLayerCount;
+	int preLayerCount;
+	int* tmpList = NULL;
+
+	struct ANN_CONFIG_STRUCT* cfgRef = config;
+	
+	log("enter");
+
+	tmpLayerCount = hiddenLayers + 2;
+	
+	// Memory allocation
+	tmpList = calloc(tmpLayerCount, sizeof(int));
+	if(tmpList == NULL)
+	{
+		retValue = ANN_MEM_FAILED;
+		goto RET;
+	}
+	else
+	{
+		tmpList[0] = cfgRef->inputs;
+		tmpList[tmpLayerCount - 1] = cfgRef->outputs;
+
+		for(i = 1; i < tmpLayerCount - 1; i++)
+			tmpList[i] = INIT_NODES;
+	}
+
+	if(cfgRef->nodeList != NULL)
+	{
+		preLayerCount = cfgRef->layers;
+
+		for(i = 1; i < preLayerCount - 1; i++)
+			tmpList[i] = cfgRef->nodeList[i];
+		
+		free(cfgRef->nodeList);
+	}
+
+	// Assign values
+	cfgRef->nodeList = tmpList;
+	cfgRef->layers = tmpLayerCount;
+
+RET:
+	log("exit");
+	return ANN_NO_ERROR;
+}
+
+int ann_config_set_nodes(ann_config_t config, int hiddenLayerIndex, int nodes)
+{
+	struct ANN_CONFIG_STRUCT* cfgRef = config;
+
+	// Checking
+	if(hiddenLayerIndex <= 1 || hiddenLayerIndex >= cfgRef->layers - 1)
+		return ANN_OUT_OF_RANGE;
+
+	cfgRef->nodeList[hiddenLayerIndex + 1] = nodes;
+
+	return ANN_NO_ERROR;
+}
+
+int ann_config_set_transfer_func(ann_config_t config, int tFuncIndex)
+{
+	struct ANN_CONFIG_STRUCT* cfgRef = config;
+	
+	cfgRef->transferFuncIndex = tFuncIndex;
+
+	return ANN_NO_ERROR;
+}
 
 int ann_set_weight(ann_t ann, int layerIndex, int preNodeIndex, int nodeIndex, double value)
 {
