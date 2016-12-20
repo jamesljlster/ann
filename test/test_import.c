781b88dabb3f072c83b5fd200062b1a17c30c589
Zheng-Ling Lai <jamesljlster@gmail.com>
2016-12-20 15:05:11 +0800

    Add ann stable test

---
+++ test/test_import.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#include <ann.h>
+
+int main(int argc, char* argv[])
+{
+	int iResult;
+	ann_t ann;
+
+	// Checking
+	if(argc <= 1)
+	{
+		printf("Assign a .vgn file to run the program\n");
+		return -1;
+	}
+
+	iResult = ann_import(&ann, argv[1]);
+	if(iResult != ANN_NO_ERROR)
+	{
+		printf("ann_config_import() failed with error: %d\n", iResult);
+		return -1;
+	}
+
+	ann_print(ann);
+
+	ann_delete(ann);
+
+	return 0;
+}
