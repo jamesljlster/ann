6c2a2d0e810a44878960303dc97047cfd1061363
Zheng-Ling Lai <jamesljlster@gmail.com>
2017-05-23 14:32:40 +0800

    Add rnn training function

--- src/ann.h
+++ src/ann.h
@@ -483,6 +483,8 @@ const char* ann_get_error_msg(int retValue);
  *	@return Constant string pointer that contains parsing message.
  */
 
+int rnn_training_gradient(ann_t ann, double** inputList, double** desireList, double** outputList, double** errList, int timeStep, double deltaLimit);
+int rnn_training_gradient_custom(ann_t ann, double learningRate, double momentumCoef, double** inputList, double** desireList, double** outputList, double** errList, int timeStep);
 void rnn_forget(ann_t ann);
 void rnn_bptt_erase(ann_t ann);
 void rnn_bptt_adjust_network(ann_t ann, double learningRate, double momentumCoef, double deltaLimit);
--- src/ann_training.c
+++ src/ann_training.c
@@ -6,6 +6,83 @@
 
 #include "debug.h"
 
+int rnn_training_gradient(ann_t ann, double** inputList, double** desireList, double** outputList, double** errList, int timeStep, double deltaLimit)
+{
+	int i, j;
+	int iResult;
+	int retValue = ANN_NO_ERROR;
+	
+	double* outputStore = NULL;
+	double* errorStore = NULL;
+
+	struct ANN_STRUCT* annRef = NULL;
+	struct ANN_CONFIG_STRUCT* cfgRef = NULL;
+
+	LOG("enter");
+
+	// Get referenct
+	annRef = ann;
+	cfgRef = &annRef->config;
+
+	// Memory allocation
+	outputStore = calloc(cfgRef->outputs * timeStep, sizeof(double));
+	errorStore = calloc(cfgRef->outputs * timeStep, sizeof(double));
+	if(outputStore == NULL || errorStore == NULL)
+	{
+		retValue = ANN_MEM_FAILED;
+		goto RET;
+	}
+
+	// Recurrent training
+	for(i = 0; i < timeStep; i++)
+	{
+		// Forward computation
+		ann_forward_computation(ann, inputList[i], &outputStore[i * cfgRef->outputs]);
+
+		// Find error
+		for(j = 0; j < cfgRef->outputs; j++)
+		{
+			errorStore[i * cfgRef->outputs + j] = desireList[i][j] - outputStore[i * cfgRef->outputs + j];
+		}
+
+		// Backpropagation
+		iResult = rnn_bptt_sum_delta(ann, &errorStore[i * cfgRef->outputs + j]);
+		if(iResult != ANN_NO_ERROR);
+		{
+			retValue = iResult;
+			goto RET;
+		}
+	}
+
+	// Adjust network
+	rnn_bptt_adjust_network(ann, cfgRef->learningRate, cfgRef->momentumCoef, deltaLimit);
+
+	// Erase
+	rnn_bptt_erase(ann);
+
+	// Copy values
+	for(i = 0; i < timeStep; i++)
+	{
+		for(j = 0; j < cfgRef->outputs; j++)
+		{
+			if(outputList != NULL)
+				outputList[i][j] = outputStore[i * cfgRef->outputs + j];
+
+			if(errList != NULL)
+				errList[i][j] = errorStore[i * cfgRef->outputs + j];
+		}
+	}
+
+RET:
+	if(outputStore != NULL)
+		free(outputStore);
+
+	if(errorStore != NULL)
+		free(errorStore);
+
+	return retValue;
+}
+
 int ann_training_gradient_custom(ann_t ann, double learningRate, double momentumCoef, double* input, double* desire, double* output, double* err)
 {
 	int i;
