a8176804dda9a5c524e07eb4a6620c7ed8504f9d
Zheng-Ling Lai <jamesljlster@gmail.com>
2017-08-08 20:41:11 +0800

    Using double layer importing: ann_import()/rnn_import() --> ann_import_struct()

--- src/ann_parse.h
+++ src/ann_parse.h
@@ -9,7 +9,7 @@ extern "C" {
 #endif
 
 int ann_parse_config(struct ANN_CONFIG_STRUCT* cfgPtr, struct ANN_FILE_STRUCT* fsPtr);
-int ann_parse_network(struct ANN_STRUCT* asPtr, struct ANN_FILE_STRUCT* fsPtr);
+int ann_parse_network(struct ANN_STRUCT* asPtr, struct ANN_FILE_STRUCT* fsPtr, int type);
 
 #ifdef __cplusplus
 }
